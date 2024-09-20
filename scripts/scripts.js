9192797dc3d3db4756349f558ca0c4516bfb1a0f
leoadobe <104100428+leoadobe@users.noreply.github.com>
2024-09-20 10:51:10 -0300

    adding experimentation

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,7 +12,47 @@ import {
   loadSections,
   loadCSS,
   sampleRUM,
+  getMetadata,
+  loadScript,
+  toCamelCase,
+  toClassName,  
 } from './aem.js';
+
+
+/** CUSTOM - Plugin Experiementation Starts */
+// Define an execution context
+const pluginContext = {
+  getAllMetadata,
+  getMetadata,
+  loadCSS,
+  loadScript,
+  sampleRUM,
+  toCamelCase,
+  toClassName,
+};
+
+const AUDIENCES = {
+  mobile: () => window.innerWidth < 600,
+  desktop: () => window.innerWidth >= 600,
+  // define your custom audiences here as needed
+};
+
+/**
+ * Gets all the metadata elements that are in the given scope.
+ * @param {String} scope The scope/prefix for the metadata
+ * @returns an array of HTMLElement nodes that match the given scope
+ */
+export function getAllMetadata(scope) {
+  return [...document.head.querySelectorAll(`meta[property^="${scope}:"],meta[name^="${scope}-"]`)]
+    .reduce((res, meta) => {
+      const id = toClassName(meta.name
+        ? meta.name.substring(scope.length + 1)
+        : meta.getAttribute('property').split(':')[1]);
+      res[id] = meta.getAttribute('content');
+      return res;
+    }, {});
+}
+/** CUSTOM - Plugin Experiementation Ends */
 
 /**
  * Builds hero block and prepends to main in a new section.
@@ -82,6 +122,18 @@ async function loadEager(doc) {
     await loadSection(main.querySelector('.section'), waitForFirstImage);
   }
 
+  /** CUSTOM - Plugin Experiementation Starts */
+  // Add below snippet early in the eager phase
+  if (getMetadata('experiment')
+    || Object.keys(getAllMetadata('campaign')).length
+    || Object.keys(getAllMetadata('audience')).length) {
+    // eslint-disable-next-line import/no-relative-packages
+    const { loadEager: runEager } = await import('../plugins/experimentation/src/index.js');
+    await runEager(document, { audiences: AUDIENCES }, pluginContext);
+  }
+  /** CUSTOM - Plugin Experiementation Ends */
+
+
   sampleRUM.enhance();
 
   try {
@@ -111,6 +163,17 @@ async function loadLazy(doc) {
 
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
   loadFonts();
+
+  /** CUSTOM - Plugin Experiementation Starts */
+  // Add below snippet at the end of the lazy phase
+  if ((getMetadata('experiment')
+    || Object.keys(getAllMetadata('campaign')).length
+    || Object.keys(getAllMetadata('audience')).length)) {
+    // eslint-disable-next-line import/no-relative-packages
+    const { loadLazy: runLazy } = await import('../plugins/experimentation/src/index.js');
+    await runLazy(document, { audiences: AUDIENCES }, pluginContext);
+  }
+  /** CUSTOM - Plugin Experiementation Ends */    
 }
 
 /**
